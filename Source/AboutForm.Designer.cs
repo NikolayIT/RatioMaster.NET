cbe32d6cc594cf4ec2e296b1f6cb6c1a7e580e8e
NikolayIT <admin@nikolay.it>
2013-11-24 21:51:40 +0200

    About form updated

--- Source/AboutForm.Designer.cs
+++ Source/AboutForm.Designer.cs
@@ -28,34 +28,19 @@ namespace RatioMaster_source
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
             this.label2 = new System.Windows.Forms.Label();
             this.lblProgramName = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.linkForum = new System.Windows.Forms.LinkLabel();
             this.lblForumAdress = new System.Windows.Forms.Label();
-            this.linkEMail2 = new System.Windows.Forms.LinkLabel();
-            this.lblInfoEMail2 = new System.Windows.Forms.Label();
             this.linkWebSite = new System.Windows.Forms.LinkLabel();
             this.lblWebSite = new System.Windows.Forms.Label();
             this.linkEMail = new System.Windows.Forms.LinkLabel();
             this.linkAuthorWebSite = new System.Windows.Forms.LinkLabel();
             this.lblInfoEMail = new System.Windows.Forms.Label();
             this.lblInfoAuthorWebSite = new System.Windows.Forms.Label();
-            this.boxOriginal = new System.Windows.Forms.GroupBox();
-            this.linkJTSmail = new System.Windows.Forms.LinkLabel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.linkRMmail = new System.Windows.Forms.LinkLabel();
-            this.lblMDmail = new System.Windows.Forms.Label();
-            this.linkMDforums = new System.Windows.Forms.LinkLabel();
-            this.linkMDsite = new System.Windows.Forms.LinkLabel();
-            this.label6 = new System.Windows.Forms.Label();
-            this.lblMDsite = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
-            this.boxOriginal.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
@@ -63,7 +48,7 @@ namespace RatioMaster_source
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(242, 63);
+            this.label2.Location = new System.Drawing.Point(114, 63);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(218, 20);
             this.label2.TabIndex = 1;
@@ -73,7 +58,7 @@ namespace RatioMaster_source
             // 
             this.lblProgramName.AutoSize = true;
             this.lblProgramName.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblProgramName.Location = new System.Drawing.Point(198, 19);
+            this.lblProgramName.Location = new System.Drawing.Point(71, 19);
             this.lblProgramName.Name = "lblProgramName";
             this.lblProgramName.Size = new System.Drawing.Size(327, 44);
             this.lblProgramName.TabIndex = 0;
@@ -83,8 +68,6 @@ namespace RatioMaster_source
             // 
             this.groupBox1.Controls.Add(this.linkForum);
             this.groupBox1.Controls.Add(this.lblForumAdress);
-            this.groupBox1.Controls.Add(this.linkEMail2);
-            this.groupBox1.Controls.Add(this.lblInfoEMail2);
             this.groupBox1.Controls.Add(this.linkWebSite);
             this.groupBox1.Controls.Add(this.lblWebSite);
             this.groupBox1.Controls.Add(this.linkEMail);
@@ -93,7 +76,7 @@ namespace RatioMaster_source
             this.groupBox1.Controls.Add(this.lblInfoAuthorWebSite);
             this.groupBox1.Location = new System.Drawing.Point(12, 98);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(527, 65);
+            this.groupBox1.Size = new System.Drawing.Size(443, 65);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Communication";
@@ -103,7 +86,7 @@ namespace RatioMaster_source
             this.linkForum.AutoSize = true;
             this.linkForum.Cursor = System.Windows.Forms.Cursors.Help;
             this.linkForum.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.linkForum.Location = new System.Drawing.Point(382, 22);
+            this.linkForum.Location = new System.Drawing.Point(272, 44);
             this.linkForum.Name = "linkForum";
             this.linkForum.Size = new System.Drawing.Size(143, 13);
             this.linkForum.TabIndex = 5;
@@ -115,38 +98,17 @@ namespace RatioMaster_source
             // 
             this.lblForumAdress.AutoSize = true;
             this.lblForumAdress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblForumAdress.Location = new System.Drawing.Point(333, 22);
+            this.lblForumAdress.Location = new System.Drawing.Point(221, 44);
             this.lblForumAdress.Name = "lblForumAdress";
             this.lblForumAdress.Size = new System.Drawing.Size(45, 13);
             this.lblForumAdress.TabIndex = 4;
             this.lblForumAdress.Text = "Forum:";
             // 
-            // linkEMail2
-            // 
-            this.linkEMail2.AutoSize = true;
-            this.linkEMail2.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkEMail2.Location = new System.Drawing.Point(225, 44);
-            this.linkEMail2.Name = "linkEMail2";
-            this.linkEMail2.Size = new System.Drawing.Size(109, 13);
-            this.linkEMail2.TabIndex = 9;
-            this.linkEMail2.TabStop = true;
-            this.linkEMail2.Text = "nrpg666@yahoo.com";
-            this.linkEMail2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkEMail2_LinkClicked);
-            // 
-            // lblInfoEMail2
-            // 
-            this.lblInfoEMail2.AutoSize = true;
-            this.lblInfoEMail2.Location = new System.Drawing.Point(181, 44);
-            this.lblInfoEMail2.Name = "lblInfoEMail2";
-            this.lblInfoEMail2.Size = new System.Drawing.Size(38, 13);
-            this.lblInfoEMail2.TabIndex = 8;
-            this.lblInfoEMail2.Text = "E-mail:";
-            // 
             // linkWebSite
             // 
             this.linkWebSite.AutoSize = true;
             this.linkWebSite.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkWebSite.Location = new System.Drawing.Point(65, 22);
+            this.linkWebSite.Location = new System.Drawing.Point(50, 22);
             this.linkWebSite.Name = "linkWebSite";
             this.linkWebSite.Size = new System.Drawing.Size(107, 13);
             this.linkWebSite.TabIndex = 1;
@@ -159,18 +121,18 @@ namespace RatioMaster_source
             this.lblWebSite.AutoSize = true;
             this.lblWebSite.Location = new System.Drawing.Point(6, 22);
             this.lblWebSite.Name = "lblWebSite";
-            this.lblWebSite.Size = new System.Drawing.Size(54, 13);
+            this.lblWebSite.Size = new System.Drawing.Size(33, 13);
             this.lblWebSite.TabIndex = 0;
-            this.lblWebSite.Text = "Web Site:";
+            this.lblWebSite.Text = "Web:";
             // 
             // linkEMail
             // 
             this.linkEMail.AutoSize = true;
             this.linkEMail.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkEMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.linkEMail.Location = new System.Drawing.Point(49, 44);
+            this.linkEMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkEMail.Location = new System.Drawing.Point(50, 44);
             this.linkEMail.Name = "linkEMail";
-            this.linkEMail.Size = new System.Drawing.Size(103, 13);
+            this.linkEMail.Size = new System.Drawing.Size(87, 13);
             this.linkEMail.TabIndex = 7;
             this.linkEMail.TabStop = true;
             this.linkEMail.Text = "admin@nikolay.it";
@@ -180,7 +142,7 @@ namespace RatioMaster_source
             // 
             this.linkAuthorWebSite.AutoSize = true;
             this.linkAuthorWebSite.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkAuthorWebSite.Location = new System.Drawing.Point(249, 22);
+            this.linkAuthorWebSite.Location = new System.Drawing.Point(272, 22);
             this.linkAuthorWebSite.Name = "linkAuthorWebSite";
             this.linkAuthorWebSite.Size = new System.Drawing.Size(79, 13);
             this.linkAuthorWebSite.TabIndex = 3;
@@ -200,146 +162,31 @@ namespace RatioMaster_source
             // lblInfoAuthorWebSite
             // 
             this.lblInfoAuthorWebSite.AutoSize = true;
-            this.lblInfoAuthorWebSite.Location = new System.Drawing.Point(203, 22);
+            this.lblInfoAuthorWebSite.Location = new System.Drawing.Point(221, 22);
             this.lblInfoAuthorWebSite.Name = "lblInfoAuthorWebSite";
             this.lblInfoAuthorWebSite.Size = new System.Drawing.Size(41, 13);
             this.lblInfoAuthorWebSite.TabIndex = 2;
             this.lblInfoAuthorWebSite.Text = "Author:";
-            // 
-            // boxOriginal
-            // 
-            this.boxOriginal.Controls.Add(this.linkJTSmail);
-            this.boxOriginal.Controls.Add(this.label5);
-            this.boxOriginal.Controls.Add(this.linkRMmail);
-            this.boxOriginal.Controls.Add(this.lblMDmail);
-            this.boxOriginal.Controls.Add(this.linkMDforums);
-            this.boxOriginal.Controls.Add(this.linkMDsite);
-            this.boxOriginal.Controls.Add(this.label6);
-            this.boxOriginal.Controls.Add(this.lblMDsite);
-            this.boxOriginal.Location = new System.Drawing.Point(12, 169);
-            this.boxOriginal.Name = "boxOriginal";
-            this.boxOriginal.Size = new System.Drawing.Size(527, 61);
-            this.boxOriginal.TabIndex = 3;
-            this.boxOriginal.TabStop = false;
-            this.boxOriginal.Text = "Moofdev";
-            // 
-            // linkJTSmail
-            // 
-            this.linkJTSmail.AutoSize = true;
-            this.linkJTSmail.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkJTSmail.Location = new System.Drawing.Point(332, 41);
-            this.linkJTSmail.Name = "linkJTSmail";
-            this.linkJTSmail.Size = new System.Drawing.Size(95, 13);
-            this.linkJTSmail.TabIndex = 7;
-            this.linkJTSmail.TabStop = true;
-            this.linkJTSmail.Text = "dev@moofdev.org";
-            this.linkJTSmail.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkJTSmail_LinkClicked);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(254, 41);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 13);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "JTS-UD mail:";
-            // 
-            // linkRMmail
-            // 
-            this.linkRMmail.AutoSize = true;
-            this.linkRMmail.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkRMmail.Location = new System.Drawing.Point(109, 41);
-            this.linkRMmail.Name = "linkRMmail";
-            this.linkRMmail.Size = new System.Drawing.Size(139, 13);
-            this.linkRMmail.TabIndex = 5;
-            this.linkRMmail.TabStop = true;
-            this.linkRMmail.Text = "ratiomaster_06@yahoo.com";
-            this.linkRMmail.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkRMmail_LinkClicked);
-            // 
-            // lblMDmail
-            // 
-            this.lblMDmail.AutoSize = true;
-            this.lblMDmail.Location = new System.Drawing.Point(6, 41);
-            this.lblMDmail.Name = "lblMDmail";
-            this.lblMDmail.Size = new System.Drawing.Size(97, 13);
-            this.lblMDmail.TabIndex = 4;
-            this.lblMDmail.Text = "RatioMaster e-mail:";
-            // 
-            // linkMDforums
-            // 
-            this.linkMDforums.AutoSize = true;
-            this.linkMDforums.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkMDforums.Location = new System.Drawing.Point(220, 20);
-            this.linkMDforums.Name = "linkMDforums";
-            this.linkMDforums.Size = new System.Drawing.Size(165, 13);
-            this.linkMDforums.TabIndex = 3;
-            this.linkMDforums.TabStop = true;
-            this.linkMDforums.Text = "http://www.moofdev.org/forums/";
-            this.linkMDforums.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkMDforums_LinkClicked);
-            // 
-            // linkMDsite
-            // 
-            this.linkMDsite.AutoSize = true;
-            this.linkMDsite.Cursor = System.Windows.Forms.Cursors.Help;
-            this.linkMDsite.Location = new System.Drawing.Point(40, 20);
-            this.linkMDsite.Name = "linkMDsite";
-            this.linkMDsite.Size = new System.Drawing.Size(129, 13);
-            this.linkMDsite.TabIndex = 1;
-            this.linkMDsite.TabStop = true;
-            this.linkMDsite.Text = "http://www.moofdev.org/";
-            this.linkMDsite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkMDsite_LinkClicked);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(175, 20);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(39, 13);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Forum:";
-            // 
-            // lblMDsite
-            // 
-            this.lblMDsite.AutoSize = true;
-            this.lblMDsite.Location = new System.Drawing.Point(6, 20);
-            this.lblMDsite.Name = "lblMDsite";
-            this.lblMDsite.Size = new System.Drawing.Size(28, 13);
-            this.lblMDsite.TabIndex = 0;
-            this.lblMDsite.Text = "Site:";
             // 
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(12, 236);
+            this.button1.Location = new System.Drawing.Point(12, 169);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(527, 27);
+            this.button1.Size = new System.Drawing.Size(443, 27);
             this.button1.TabIndex = 5;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(160, 80);
-            this.pictureBox1.TabIndex = 115;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 275);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(467, 209);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.boxOriginal);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblProgramName);
@@ -351,9 +198,6 @@ namespace RatioMaster_source
             this.Load += new System.EventHandler(this.AboutForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.boxOriginal.ResumeLayout(false);
-            this.boxOriginal.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -366,24 +210,12 @@ namespace RatioMaster_source
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.LinkLabel linkForum;
         private System.Windows.Forms.Label lblForumAdress;
-        private System.Windows.Forms.LinkLabel linkEMail2;
-        private System.Windows.Forms.Label lblInfoEMail2;
         private System.Windows.Forms.LinkLabel linkWebSite;
         private System.Windows.Forms.Label lblWebSite;
         private System.Windows.Forms.LinkLabel linkEMail;
         private System.Windows.Forms.LinkLabel linkAuthorWebSite;
         private System.Windows.Forms.Label lblInfoEMail;
         private System.Windows.Forms.Label lblInfoAuthorWebSite;
-        private System.Windows.Forms.GroupBox boxOriginal;
-        private System.Windows.Forms.LinkLabel linkJTSmail;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.LinkLabel linkRMmail;
-        private System.Windows.Forms.Label lblMDmail;
-        private System.Windows.Forms.LinkLabel linkMDforums;
-        private System.Windows.Forms.LinkLabel linkMDsite;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label lblMDsite;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
