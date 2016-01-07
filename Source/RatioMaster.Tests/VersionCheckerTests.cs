af07f0036172c6dcaa103621a4174a3145a70fa6
Nikolay Kostov <admin@nikolay.it>
2016-01-07 22:55:28 +0200

    Added first unit tests GetServerVersionIdShouldReturnExactlyFourCharacters

---
+++ Source/RatioMaster.Tests/VersionCheckerTests.cs
@@ -0,0 +1,21 @@
+﻿namespace RatioMaster.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using RatioMaster_source;
+
+    [TestFixture]
+    public class VersionCheckerTests
+    {
+        [Test]
+        public void GetServerVersionIdShouldReturnExactlyFourCharacters()
+        {
+            var versionChecker = new VersionChecker(string.Empty);
+            var serverVersion = versionChecker.GetServerVersionId();
+            Console.WriteLine(serverVersion);
+            Assert.AreEqual(4, serverVersion.Length);
+        }
+    }
+}
